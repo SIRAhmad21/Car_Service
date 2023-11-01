2e9657671b5c18ba47e378ad35607f75a5c039e6
Sahmad <sayedahmadalrefai@gmail.com>
2023-11-02 02:58:51 +0300

    Update2

--- CarServiceUI/Controllers/AccountController.cs
+++ CarServiceUI/Controllers/AccountController.cs
@@ -66,11 +66,12 @@ namespace CarServiceUI.Controllers
         {
             if (ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(model.Email!, model.Password!, model.RememberMe, false);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "Home");
                 }
+                    ModelState.AddModelError("", "Invailed uaernamwe or password");            
                 ModelState.AddModelError("", "Invailed uaernamwe or password");
                 return View(model);
             }
